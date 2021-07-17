a96c4cfeef340b47b9a70dda3e78d08589c67340
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-07-17 21:35:25 +0300

    Create avatar.js

---
+++ avatar.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+
+exports.run = function(client, message, args) {
+  
+const codework = message.mentions.users.first()
+let user;
+  
+if (message.mentions.users.first())  {user = message.mentions.users.first();}
+  
+else {user = message.author;}
+  
+return message.channel.send(new Discord.MessageEmbed()
+                            
+.setDescription(`**${user.tag}**`)
+.setColor(`RANDOM`)
+.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+.setImage(user.displayAvatarURL({ dynamic: true })))
+  
+};
+exports.conf = {
+    enabled: false,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'avatar'
+  };
